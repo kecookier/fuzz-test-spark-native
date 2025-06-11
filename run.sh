8519dbea3ba295988a6bd8e25beced7e33ddb5f5
zhaokuo03 <zhaokuo03@meituan.com>
2025-06-11 17:55:42 +0800

    followup

--- run.sh
+++ run.sh
@@ -1,1 +1,10 @@
-scala -cp target/classes:$(mvn dependency:build-classpath -Dmdep.outputFile=/dev/stdout -q) org.apache.gluten.fuzz.SparkFunctionAnalyzer
+#!/bin/bash
+
+export SPARK_HOME=/opt/meituan/zhaokuo03/dev/spark-3.5
+SPARK_MASTER=local
+$SPARK_HOME/bin/spark-submit \
+    --master $SPARK_MASTER \
+    --class org.apache.gluten.fuzz.SparkFunctionAnalyzer \
+    target/fuzz-test-spark-native-1.0-SNAPSHOT.jar
+
+#scala -cp target/classes:$(mvn dependency:build-classpath -Dmdep.outputFile=/dev/stdout -q) org.apache.gluten.fuzz.SparkFunctionAnalyzer
